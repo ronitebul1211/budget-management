a93ea89eddea130a4f4bf6dfa56dc11ffb745724
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-06 16:45:23 +0200

    add option to display error message on input field

--- src/components/_InputFields/InputField.js
+++ src/components/_InputFields/InputField.js
@@ -5,20 +5,24 @@ import PropTypes from "prop-types";
 /**
  For Valid Types, check Prop Types
  
- # value -> initial input value
+ # value -> initial input value (required)
  # config.label -> field label (required)
    config.displayMode -> display mode of the label relative to the input field (required)
    config.name -> input name (event.target.name) (required)
    config.type -> input type (required)
    config.limitMin -> limit input type html attribute (optional)
    config.limitMax -> limit input type html attribute (optional)
+ # error.message -> string message to display when input error (optional)
+   error.isDisplayed -> boolean set display / hide (optional)
  # onChangeCallback -> reference to callback function that handle on change event (required)
 
  */
 
-const InputField = ({ value, config, onChangeCallback }) => {
+const InputField = ({ value, config, error, onChangeCallback }) => {
+   console.log(config.name + error);
+
    return (
-      <div className={`field field--${config.displayMode}`}>
+      <div className={`field field--${config.displayMode} ${error && error.isDisplayed && "field--error"}`}>
          <label className="field__label-section">{config.label}</label>
          <input
             className="field__input-section field__input-area"
@@ -29,6 +33,7 @@ const InputField = ({ value, config, onChangeCallback }) => {
             {...(config.limitMin ? { min: config.limitMin } : {})}
             {...(config.limitMax ? { max: config.limitMax } : {})}
          />
+         {error && error.isDisplayed && <div className="field__error-message">{error.message}</div>}
       </div>
    );
 };
@@ -42,6 +47,10 @@ InputField.propTypes = {
       type: PropTypes.oneOf(["text", "number", "date"]).isRequired,
       limitMin: PropTypes.string,
       limitMax: PropTypes.string,
+   }).isRequired,
+   error: PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      isDisplayed: PropTypes.bool.isRequired,
    }),
    onChangeCallback: PropTypes.func.isRequired,
 };
