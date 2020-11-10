678098bebd5270ab2cbb99c4cf60c35e4a6640a2
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-10 11:25:03 +0200

    refactor css class name, get current data as props

--- src/components/MonthStatusView/MonthStatusView.js
+++ src/components/MonthStatusView/MonthStatusView.js
@@ -1,41 +1,37 @@
 import React from "react";
 import "./MonthStatusView.css";
-import { getCurrentMonth, getCurrentYear, getMonthNameInHebrew } from "../../utilities/functions";
+import { getMonthNameInHebrew } from "../../utilities/functions";
 //Components
 
 //FIXME: add prop types + refactor by conventions.txt
 
 import ButtonIcon from "../_Buttons/ButtonIcon";
 
-const MonthStatus = ({ monthStatusData, handleAddTransactionClickCallback }) => {
+const MonthStatus = ({ currentDate, data, onButtonClickCallback }) => {
    return (
       <div className="month-status">
-         <h1 className="month-status__title">{`${getMonthNameInHebrew(
-            getCurrentMonth(),
-         )}, ${getCurrentYear()}`}</h1>
-         <div className="month-status__status-section">
-            <div className="month-status__status-item">
-               <span className="month-status__status-item-title">תקציב</span>
-               <span className="month-status__status-item-content">{monthStatusData.credit} &#x20aa;</span>
+         <h1 className="month-status__title">
+            {`${getMonthNameInHebrew(currentDate.month)}, ${currentDate.year}`}
+         </h1>
+
+         <div className="month-status__content">
+            <div className="month-status__item">
+               <span className="month-status__item-title">תקציב</span>
+               <span className="month-status__item-content">{data.credit} &#x20aa;</span>
             </div>
-            <div className="month-status__status-item">
-               <span className="month-status__status-item-title">הוצאות</span>
-               <span className="month-status__status-item-content--debit">
-                  {monthStatusData.debit} &#x20aa;
-               </span>
+            <div className="month-status__item">
+               <span className="month-status__item-title">הוצאות</span>
+               <span className="month-status__item-content--debit">{data.debit} &#x20aa;</span>
             </div>
 
-            <div className="month-status__status-item">
-               <span className="month-status__status-item-title">יתרה</span>
-               <span
-                  className={`month-status__status-item-content--${
-                     monthStatusData.balance > 0 ? "credit" : "debit"
-                  }`}>
-                  {monthStatusData.balance} &#x20aa;
+            <div className="month-status__item">
+               <span className="month-status__item-title">יתרה</span>
+               <span className={`month-status__item-content--${data.balance > 0 ? "credit" : "debit"}`}>
+                  {data.balance} &#x20aa;
                </span>
             </div>
-            <div className="month-status__add-btn-container">
-               <ButtonIcon type="add" size="big" clickHandlerCallback={handleAddTransactionClickCallback} />
+            <div className="month-status__btn-container">
+               <ButtonIcon type="add" size="big" clickHandlerCallback={onButtonClickCallback} />
             </div>
          </div>
       </div>
