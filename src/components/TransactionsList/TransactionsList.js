b1d8580077375a88ca50018afb2d0d9003eb79e8
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-11 20:37:53 +0200

    split rendering in transactions list component to two separate functions

--- src/components/TransactionsList/TransactionsList.js
+++ src/components/TransactionsList/TransactionsList.js
@@ -7,62 +7,68 @@ import ButtonIcon from "../_Buttons/ButtonIcon";
 //FIXME: add prop types + refactor by conventions.txt
 
 const TransactionsList = ({ transactionsListData, isEditableList, onListEventCallback }) => {
-   const renderedTableRows = transactionsListData.map((transaction) => {
+   const renderHeadlines = () => {
       return (
-         <tr className="transaction-table__row" key={transaction._id}>
-            <td className="transaction-table__item">{dates.getAbbreviatedDate(transaction.date)} </td>
-            <td className="transaction-table__item">{transaction.paymentMethod}</td>
-            <td className="transaction-table__item">{transaction.category}</td>
-            <td className="transaction-table__item">{transaction.description}</td>
-            <td className="transaction-table__item">
-               <span className={`transaction-table__payment-item--${transaction.type}`}>
-                  {transaction.totalPayment} &#x20aa;
-               </span>
-            </td>
+         <tr className="transaction-table__row">
+            <th className="transaction-table__headline">תאריך</th>
+            <th className="transaction-table__headline">אמצעי תשלום</th>
+            <th className="transaction-table__headline">קטגוריה</th>
+            <th className="transaction-table__headline">תיאור</th>
+            <th className="transaction-table__headline">סכום</th>
             {isEditableList ? (
                <React.Fragment>
-                  <td className="transaction-table__item">
-                     <ButtonIcon
-                        type="edit"
-                        size="small"
-                        clickHandlerCallback={() => {
-                           onListEventCallback("OPEN_FORM", transaction);
-                        }}
-                     />
-                  </td>
-                  <td className="transaction-table__item">
-                     <ButtonIcon
-                        type="delete"
-                        size="small"
-                        clickHandlerCallback={() => {
-                           onListEventCallback("DELETE_TRANSACTION", transaction);
-                        }}
-                     />
-                  </td>
+                  <th className="transaction-table__headline"></th>
+                  <th className="transaction-table__headline"></th>
                </React.Fragment>
             ) : null}
          </tr>
       );
-   });
+   };
 
-   return (
-      <table className={`transaction-table transaction-table--${isEditableList ? "edit" : "read"}`}>
-         <thead className="transaction-table__headlines">
-            <tr className="transaction-table__row">
-               <th className="transaction-table__headline">תאריך</th>
-               <th className="transaction-table__headline">אמצעי תשלום</th>
-               <th className="transaction-table__headline">קטגוריה</th>
-               <th className="transaction-table__headline">תיאור</th>
-               <th className="transaction-table__headline">סכום</th>
+   const renderRows = () => {
+      return transactionsListData.map((transaction) => {
+         return (
+            <tr className="transaction-table__row" key={transaction._id}>
+               <td className="transaction-table__item">{dates.getAbbreviatedDate(transaction.date)} </td>
+               <td className="transaction-table__item">{transaction.paymentMethod}</td>
+               <td className="transaction-table__item">{transaction.category}</td>
+               <td className="transaction-table__item">{transaction.description}</td>
+               <td className="transaction-table__item">
+                  <span className={`transaction-table__payment-item--${transaction.type}`}>
+                     {transaction.totalPayment} &#x20aa;
+                  </span>
+               </td>
                {isEditableList ? (
                   <React.Fragment>
-                     <th className="transaction-table__headline"></th>
-                     <th className="transaction-table__headline"></th>
+                     <td className="transaction-table__item">
+                        <ButtonIcon
+                           type="edit"
+                           size="small"
+                           clickHandlerCallback={() => {
+                              onListEventCallback("OPEN_FORM", transaction);
+                           }}
+                        />
+                     </td>
+                     <td className="transaction-table__item">
+                        <ButtonIcon
+                           type="delete"
+                           size="small"
+                           clickHandlerCallback={() => {
+                              onListEventCallback("DELETE_TRANSACTION", transaction);
+                           }}
+                        />
+                     </td>
                   </React.Fragment>
                ) : null}
             </tr>
-         </thead>
-         <tbody className="transaction-table__body">{renderedTableRows}</tbody>
+         );
+      });
+   };
+
+   return (
+      <table className={`transaction-table transaction-table--${isEditableList ? "edit" : "read"}`}>
+         <thead className="transaction-table__headlines">{renderHeadlines()}</thead>
+         <tbody className="transaction-table__body">{renderRows()}</tbody>
       </table>
    );
 };
