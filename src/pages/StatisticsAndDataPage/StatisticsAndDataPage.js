1883c719f0fa80173b76e53a05312df15d5e65f4
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-12 13:28:27 +0200

    reset statistics and data component page

--- src/pages/StatisticsAndDataPage/StatisticsAndDataPage.js
+++ src/pages/StatisticsAndDataPage/StatisticsAndDataPage.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./StatisticsAndDataPage.css";
-import dates from "../../utilities/dates";
-import { getDebitData, sortTransactionsByDate } from "../../utilities/functions";
 
 //Components
 import PieGraph from "../../components/_Graphs/PieGraph";
@@ -13,115 +11,73 @@ import TransactionsList from "../../components/TransactionsList/TransactionsList
 //TODO: render month options dynamically, category match month
 
 class StatisticsAndDataPage extends React.Component {
-   // state = {
-   //    currentMonthTransactions: [],
-   //    selectedMonthId: "",
-   //    monthSelection: { id: 0, options: [], transactions: [] },
-   //    graphData: { labels: [], dataset: [] },
-   // };
+   /**
+    State = {
+       transactionListData: []
+       distributionOfDebitByCategory: {קניות: 0, אוכל בחוץ:500}
 
-   // //TODO: update state once, extract functions
-   // async componentDidMount() {
-   //    //Get transactions record generate options
-   //    const transactionsRecords = await getHistory();
-   //    const monthSelectionOptions = transactionsRecords.map((transactionsRecord) => {
-   //       return { label: dates.getHebrewMonthName(transactionsRecord.month), value: transactionsRecord.id };
-   //    });
-   //    //First month - default
-   //    const defaultTransactionsId = monthSelectionOptions[transactionsRecords.length - 1].value;
-   //    const defaultMonthTransactionsData = await getTransactionsData(defaultTransactionsId);
-   //    sortTransactionsByDate(defaultMonthTransactionsData);
-   //    this.setState(
-   //       {
-   //          monthSelection: {
-   //             id: defaultTransactionsId,
-   //             options: monthSelectionOptions,
-   //             transactions: defaultMonthTransactionsData,
-   //          },
-   //       },
-   //       () => console.log(this.state),
-   //    );
-   //    const debitData = getDebitData(defaultMonthTransactionsData);
-   //    this.setState({ graphData: { labels: debitData.labels, dataset: debitData.dataset } });
-   // }
+    }
+    */
+   state = {
+      currentMonthTransactions: [],
+      monthSelection: { id: 0, options: [], transactions: [] },
+      graphData: { labels: [], dataset: [] },
+   };
 
-   // onInputChange = async (event) => {
-   //    if (event.target.name === "monthSelection") {
-   //       //set state with month id, to update month selection ui
-   //       const selectedMonthId = event.target.value;
-   //       const monthSelection = { ...this.state.monthSelection };
-   //       monthSelection.id = selectedMonthId;
-   //       this.setState({ monthSelection });
+   //TODO: update state once, extract functions
+   async componentDidMount() {}
 
-   //       //set state with transaction data and graph data to update transactions & graph views
-   //       //Transactions data
-   //       const transactionsData = await getTransactionsData(selectedMonthId);
-   //       sortTransactionsByDate(transactionsData);
-   //       monthSelection.transactions = transactionsData;
-   //       //Graph data
-   //       const debitData = getDebitData(transactionsData);
-   //       this.setState(
-   //          { graphData: { labels: debitData.labels, dataset: debitData.dataset }, monthSelection },
-   //          () => {
-   //             console.log(this.state);
-   //          },
-   //       );
-   //    }
-   // };
+   onInputChange = async (event) => {
+      if (event.target.name === "monthSelection") {
+         //set state with month id, to update month selection ui
+         //set state with transaction data and graph data to update transactions & graph views
+         //Transactions data
+         //Graph data
+      }
+   };
 
-   // sortTransaction = () => {};
+   sortTransaction = () => {};
 
    //TODO: change class name for list view container
-   // render() {
-   //    return (
-   //       <div className="statis-test-container">
-   //          <h2>תמונת מצב חודשית</h2>
-   //          <div className="graph-section">
-   //             <SelectField
-   //                value={this.state.monthSelection.id}
-   //                options={this.state.monthSelection.options}
-   //                config={{ fieldLabel: "בחר חודש", inputName: "monthSelection", displayMode: "row" }}
-   //                onChangeCallback={this.onInputChange}
-   //             />
-   //             <PieGraph
-   //                title={"התפלגות הוצאות לפי קטגוריה"}
-   //                labels={this.state.graphData.labels}
-   //                dataset={this.state.graphData.dataset}
-   //                backgroundColors={["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"]}
-   //             />
-   //          </div>
-
-   //          <div className="list-container">
-   //             <div className="test-select">
-   //                <SelectField
-   //                   value=""
-   //                   options={[
-   //                      { label: "תאריך", value: "חשבונות" },
-   //                      { label: "אמצעי תשלום", value: "קניות" },
-   //                      { label: "קטגוריה", value: "בילויים" },
-   //                      { label: "סכום", value: "בילויים" },
-   //                   ]}
-   //                   config={{ fieldLabel: "מיין לפי", inputName: "test", displayMode: "row" }}
-   //                   onChangeCallback={this.sortTransaction}
-   //                />
-   //             </div>
-
-   //             <TransactionListView transactions={this.state.monthSelection.transactions} mode="read" />
-   //          </div>
-   //       </div>
-   //    );
-   // }
-
    render() {
-      return <div>Statistics and data</div>;
+      return (
+         <div className="statis-test-container">
+            <h2>תמונת מצב חודשית</h2>
+            <div className="graph-section">
+               <SelectField
+                  value={this.state.monthSelection.id}
+                  options={this.state.monthSelection.options}
+                  config={{ fieldLabel: "בחר חודש", inputName: "monthSelection", displayMode: "row" }}
+                  onChangeCallback={this.onInputChange}
+               />
+               <PieGraph
+                  title={"התפלגות הוצאות לפי קטגוריה"}
+                  labels={this.state.graphData.labels}
+                  dataset={this.state.graphData.dataset}
+                  backgroundColors={["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"]}
+               />
+            </div>
+
+            <div className="list-container">
+               <div className="test-select">
+                  <SelectField
+                     value=""
+                     options={[
+                        { label: "תאריך", value: "חשבונות" },
+                        { label: "אמצעי תשלום", value: "קניות" },
+                        { label: "קטגוריה", value: "בילויים" },
+                        { label: "סכום", value: "בילויים" },
+                     ]}
+                     config={{ fieldLabel: "מיין לפי", inputName: "test", displayMode: "row" }}
+                     onChangeCallback={this.sortTransaction}
+                  />
+               </div>
+
+               <TransactionsList transactionsListData={[]} isEditableList={false} />
+            </div>
+         </div>
+      );
    }
 }
 
 export default StatisticsAndDataPage;
-/** Select Field Component:
-# Props:
-- value - string - initial value
-- options - array of option obj - {label: '', value: ''}
-- config - config object - {fieldLabel: "", inputName: "", displayMode: "column" / "row" }
-- onChangeCallback - function ref - to get update when option input change         
-*/
