a0a7849aa2681abdf1eb1fe80bf00c535d3d212d
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-18 11:08:44 +0200

    refactor pie graph hooks component

--- src/components/_Graphs/PieGraphHooks.js
+++ src/components/_Graphs/PieGraphHooks.js
@@ -2,33 +2,26 @@ import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js";
 import "./PieGraph.css";
 
+//FIXME: PROP TYPE
+
 const PieGraph = (props) => {
-   const graphCanvasRef = useRef(null);
-   const [graph, setGraph] = useState(null);
+   const canvasRef = useRef(null);
+   const graphRef = useRef(null);
 
    useEffect(() => {
-      if (!graph) {
-         const labelsAndDataset = extractLabelsAndDataset(props.data);
-         return setGraph(buildGraph(labelsAndDataset));
-      }
-
-      const updateData = (action, labels = [], dataset = []) => {
-         graph.data.labels = labels;
-         graph.data.datasets.forEach((graphDataset) => {
-            if (action === "REMOVE_DATA") {
-               graphDataset.data = dataset;
-            } else {
-               graphDataset.data.push(...dataset);
-            }
-         });
-         graph.update();
-      };
-
       const { labels, dataset } = extractLabelsAndDataset(props.data);
-      updateData("REMOVE_DATA");
-      updateData("NEW_DATA", labels, dataset);
-   }, [props.data, graph]);
+      if (graphRef.current) {
+         updateData();
+         updateData(labels, dataset);
+      } else {
+         graphRef.current = buildGraph(labels, dataset);
+      }
+   }, [props.data]);
 
+   /**
+    * @param {object} data object keys represent labels, object values represent data
+    * @return {object} object contain labels, dataset array
+    */
    const extractLabelsAndDataset = (data) => {
       const labelsAndDataset = { labels: [], dataset: [] };
       for (const property in data) {
@@ -38,18 +31,22 @@ const PieGraph = (props) => {
       return labelsAndDataset;
    };
 
-   const buildGraph = (labelsAndDataset) => {
+   /**
+    * @param {array} labels array of strings represent graph labels
+    * @param {array} dataset array of numbers represent graph dataset
+    * @return {object} reference to graph object
+    */
+   const buildGraph = (labels, dataset) => {
       Chart.defaults.global.defaultFontFamily = "'Alef', 'sans - serif'";
       const colors = ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#d145f7", "#45f7b9", "#e8d7b4"];
-      const ctx = graphCanvasRef.current.getContext("2d");
-
+      const ctx = canvasRef.current.getContext("2d");
       const graph = new Chart(ctx, {
          type: "pie",
          data: {
-            labels: labelsAndDataset.labels,
+            labels: labels,
             datasets: [
                {
-                  data: labelsAndDataset.dataset,
+                  data: dataset,
                   backgroundColor: colors,
                },
             ],
@@ -79,117 +76,26 @@ const PieGraph = (props) => {
       return graph;
    };
 
+   /**
+    * Update graph data, when no args passed in reset the table
+    * @param {array} labels array of strings represent graph labels
+    * @param {array} dataset array of numbers represent graph dataset
+    */
+   const updateData = (labels = [], dataset = []) => {
+      graphRef.current.data.labels = labels;
+      graphRef.current.data.datasets.forEach((graphDataset) => {
+         graphDataset.data = dataset;
+      });
+      graphRef.current.update();
+   };
+
    return (
       <div className="pie-graph">
          <div className="pie-graph__container">
-            <canvas ref={graphCanvasRef} />
+            <canvas ref={canvasRef} />
          </div>
       </div>
    );
 };
 
-// class PieGraph extends React.Component {
-//    constructor() {
-//       super();
-//       this.pieCanvasRef = React.createRef();
-//       this.labelsAndDataset = null;
-//       this.PieGraph = null;
-//       this.colors = ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#d145f7", "#45f7b9", "#e8d7b4"];
-//    }
-
-//    componentDidMount() {
-//       this.labelsAndDataset = this.extractLabelsAndDataset(this.props.data);
-//       this.buildPieGraph();
-//    }
-
-//    componentDidUpdate(prevProps) {
-//       if (!this.isIdenticalData(prevProps.data, this.props.data)) {
-//          this.labelsAndDataset = this.extractLabelsAndDataset(this.props.data);
-//          this.removeGraphData();
-//          this.updateGraphData();
-//       }
-//    }
-
-//    isIdenticalData = (obj1, obj2) => {
-//       return JSON.stringify(obj1) === JSON.stringify(obj2); // true
-//    };
-
-//    /** Update pie graph current props value: labels, dataset */
-//    updateGraphData() {
-//       this.PieGraph.data.labels = this.labelsAndDataset.labels;
-//       this.PieGraph.data.datasets.forEach((dataset) => {
-//          dataset.data.push(...this.labelsAndDataset.dataset);
-//       });
-//       this.PieGraph.update();
-//    }
-
-//    /** Remove from pie graph current labels, dataset */
-//    removeGraphData() {
-//       this.PieGraph.data.labels = [];
-//       this.PieGraph.data.datasets.forEach((dataset) => {
-//          dataset.data = [];
-//       });
-//       this.PieGraph.update();
-//    }
-
-//    /** Build pie graph inside canvas and hold ref this.PieGraph */
-//    buildPieGraph = () => {
-//       Chart.defaults.global.defaultFontFamily = "'Alef', 'sans - serif'";
-
-//       const ctx = this.pieCanvasRef.current.getContext("2d");
-
-//       this.PieGraph = new Chart(ctx, {
-//          type: "pie",
-//          data: {
-//             labels: this.labelsAndDataset.labels,
-//             datasets: [
-//                {
-//                   data: this.labelsAndDataset.dataset,
-//                   backgroundColor: this.colors,
-//                },
-//             ],
-//          },
-//          options: {
-//             legend: {
-//                position: "right",
-//                rtl: true,
-//                labels: { fontColor: "black", fontSize: 14 },
-//             },
-//             tooltips: {
-//                rtl: true,
-//                displayColors: true,
-//                xPadding: 5,
-//                yPadding: 5,
-//                callbacks: {
-//                   label: (tooltipItem, data) => {
-//                      const labels = data.labels;
-//                      const dataset = data.datasets[0].data;
-//                      const currentIndex = tooltipItem.index;
-//                      return ` ${dataset[currentIndex]} \u20AA ${labels[currentIndex]}`;
-//                   },
-//                },
-//             },
-//          },
-//       });
-//    };
-
-//    extractLabelsAndDataset = (data) => {
-//       const labelsAndDataset = { labels: [], dataset: [] };
-//       for (const property in data) {
-//          labelsAndDataset.labels.push(property);
-//          labelsAndDataset.dataset.push(data[property]);
-//       }
-//       return labelsAndDataset;
-//    };
-
-//    render() {
-//       return (
-//          <div className="pie-graph">
-//             <div className="pie-graph__container">
-//                <canvas ref={this.pieCanvasRef} />
-//             </div>
-//          </div>
-//       );
-//    }
-// }
 export default PieGraph;
