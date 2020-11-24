a9979b7ef4eabc9d2abad1565bbb7f89a4052022
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-24 18:28:40 +0200

    handle POST PUT DELETE newtork requests on custom hook

--- src/utils/custom-hook/useTransactionsApi.js
+++ src/utils/custom-hook/useTransactionsApi.js
@@ -7,11 +7,44 @@ const useTransactionsApi = () => {
    const [networkRequest, setNetworkRequestNew] = useState({ type: null, payload: null });
 
    useEffect(() => {
-      console.log("custom: new network request");
+      console.log("CUSTOM: networkRequest - useEffect");
+      const sendRequest = async () => {
+         console.log("CUSTOM: send new request");
+         try {
+            switch (networkRequest.type) {
+               case "CREATE_TRANSACTION_ENDPOINT":
+                  await transactionsApi.postTransaction(networkRequest.payload);
+                  break;
+               case "UPDATE_TRANSACTION_ENDPOINT":
+                  await transactionsApi.updateTransaction(networkRequest.payload);
+                  break;
+               case "DELETE_TRANSACTION_ENDPOINT":
+                  await transactionsApi.deleteTransaction(networkRequest.payload);
+                  break;
+               default:
+                  const invalidNetworkAction = new Error("Network request set with invalid action type");
+                  invalidNetworkAction.name = "INVALID_NETWORK_ACTION";
+                  throw invalidNetworkAction;
+            }
+            setIsMonthDataUpdated(false);
+         } catch (err) {
+            if (err.name === "INVALID_NETWORK_ACTION") {
+               throw err;
+            }
+            console.log(err);
+         }
+      };
+
+      if (networkRequest.type && networkRequest.payload) {
+         sendRequest();
+         setNetworkRequestNew({ type: null, payload: null });
+      }
    }, [networkRequest]);
 
    useEffect(() => {
+      console.log("CUSTOM: isMonthDataUpdated - useEffect ");
       const fetchMonthData = async () => {
+         console.log("CUSTOM: fetch monthData");
          try {
             const res = await transactionsApi.getMonthData("monthStatus");
             if (res.status === 204) {
