e63b5b45bcb5d4eed059feed5dea5e1912c996e8
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-24 18:13:55 +0200

    when month data isn't updated, fetch data from the api

--- src/utils/custom-hook/useTransactionsApi.js
+++ src/utils/custom-hook/useTransactionsApi.js
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
+import transactionsApi from "../../utils/transactionsApi";
 
 const useTransactionsApi = () => {
    const [monthDataNew, setMonthData] = useState();
    const [isMonthDataUpdatedNew, setIsMonthDataUpdated] = useState(false);
-   const [networkRequest, setNetworkRequestNew] = useState({ type: undefined, payload: undefined });
+   const [networkRequest, setNetworkRequestNew] = useState({ type: null, payload: null });
 
    useEffect(() => {
       console.log("custom: new network request");
    }, [networkRequest]);
 
    useEffect(() => {
-      console.log("custom: month data request");
+      const fetchMonthData = async () => {
+         try {
+            const res = await transactionsApi.getMonthData("monthStatus");
+            if (res.status === 204) {
+               return setMonthData({ transactionsList: [], status: { credit: 0, debit: 0, balance: 0 } });
+            }
+            const { monthStatus, transactionsList } = res.data;
+            return setMonthData({ transactionsList: transactionsList.data, status: monthStatus });
+         } catch (err) {
+            console.log(err);
+         }
+      };
+
+      if (!isMonthDataUpdatedNew) {
+         fetchMonthData();
+         setIsMonthDataUpdated(true);
+      }
    }, [isMonthDataUpdatedNew]);
 
    return [monthDataNew, setNetworkRequestNew];
