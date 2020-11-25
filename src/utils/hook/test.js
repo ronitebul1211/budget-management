5cfd0beb45c14ac6bc7f5a6c49d5cb42108f1f2c
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-25 15:32:08 +0200

    commit changes - master

---
+++ src/utils/hook/test.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+/**
+ *
+ *
+ * show error -
+ *
+ */
+//initialState - Error string / null
+// const useErrorHandler = (initialState) => {
+//    const [error, setError] = React.useState(initialState);
+
+//    //errorMessage - Error string, set error for 3 seconds then set null
+//    //component that use hook can render by error value
+//    const showError = (errorMessage) => {
+//       setError(errorMessage);
+//       window.setTimeout(() => {
+//          setError(null);
+//       }, 3000);
+//    };
+
+//    return { error, showError };
+// };
+
+// export default useErrorHandler;
+
+// const { error, showError } = useErrorHandler("error string");
