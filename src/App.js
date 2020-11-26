d64b27f583c7bc24858cfa4803cc5f4a73d5f8f8
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-26 15:04:01 +0200

    use statistic and data page functional component and remove old class component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import MonthManagerPage from "./pages/MonthManagerPage/MonthManagerPage";
 import StatisticsAndDataPage from "./pages/StatisticsAndDataPage/StatisticsAndDataPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-import StatisticsHooks from "./pages/StatisticsAndDataPage/StatisticsHooks";
-
 const App = () => {
    return (
       <React.Fragment>
@@ -19,7 +17,7 @@ const App = () => {
                <PrivateRoute path="/current-month" component={MonthManagerPage} />
                <PrivateRoute path="/statistics" component={StatisticsAndDataPage} />
                <Route path="/test/current-month" exact component={MonthManagerPage} />
-               <Route path="/test/statistics" exact component={StatisticsHooks} />
+               <Route path="/test/statistics" exact component={StatisticsAndDataPage} />
                <Route path="/" exact component={HomePage} />
                <Route path="*" exact component={NotFoundPage} />
             </Switch>
--- src/pages/StatisticsAndDataPage/StatisticsAndDataPage.js
+++ src/pages/StatisticsAndDataPage/StatisticsAndDataPage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./StatisticsAndDataPage.css";
 import dates from "../../utils/dates";
-import transactionsApi from "../../utils/transactionsApi";
+import { sortTransactions } from "../../utils/sortHelper";
+import useTransactionsApi from "../../utils/custom-hook/useTransactionsApi";
+import { netReqAction } from "../../utils/constants";
 //Components
 import PieGraphHooks from "../../components/_Graphs/PieGraphHooks";
 import SelectField from "../../components/_InputFields/SelectField";
@@ -11,123 +13,109 @@ import TransactionsList from "../../components/TransactionsList/TransactionsList
 
 //TODO: render month options dynamically, category match month
 
-class StatisticsAndDataPage extends React.Component {
-   state = {
-      datePicker: { month: undefined, year: undefined },
+const StatisticsAndDataPage = () => {
+   const INITIAL_STATE = {
       transactionsList: [],
-      debitDistribution: {},
+      metadata: {},
    };
+   const [{ monthData, isLoading, isError }, setNetworkRequest] = useTransactionsApi({
+      defaultState: INITIAL_STATE,
+      fetchQuery: "debitDistribution",
+   });
+   const [datePicker, setDatePicker] = useState(() => {
+      const { month, year } = dates.getDateData();
+      return { month, year };
+   });
+   const [sortedTransactionList, setSortedTransactionList] = useState(monthData.transactionsList);
+   const [sortByPicker, setSortByPicker] = useState("date");
 
-   componentDidMount = () => {
-      const currentDate = dates.getDateData();
-      this.setState({ datePicker: { month: currentDate.month, year: currentDate.year } });
-   };
+   useEffect(() => {
+      setNetworkRequest({ type: netReqAction.FETCH_TRANSACTIONS_ENDPOINT, payload: datePicker });
+   }, [datePicker, setNetworkRequest]);
 
-   componentDidUpdate = async (prevProps, prevState) => {
-      if (
-         prevState.datePicker.month !== this.state.datePicker.month ||
-         prevState.datePicker.year !== this.state.datePicker.year
-      ) {
-         const { month, year } = this.state.datePicker;
-         await this.loadMonthDataFromEndpoint(month, year);
-      }
-   };
+   useEffect(() => {
+      setSortedTransactionList(monthData.transactionsList);
+      setSortByPicker("date");
+   }, [monthData]);
 
-   onInputChange = async (e) => {
+   const onInputChange = (e) => {
       const target = e.target;
-      if (target.name === "month" || target.name === "year") {
-         this.setState((prevState) => ({
-            datePicker: {
-               ...prevState.datePicker,
-               [target.name]: parseInt(target.value),
-            },
-         }));
+      switch (target.name) {
+         case "month":
+         case "year":
+            return setDatePicker((prevState) => {
+               return {
+                  ...prevState,
+                  [target.name]: parseInt(target.value),
+               };
+            });
+         case "sortBy":
+            setSortByPicker(target.value);
+            return setSortedTransactionList(sortTransactions(sortedTransactionList, target.value));
+         default:
+            throw new Error("Invalid target name");
       }
    };
 
-   /** Load updated data from endpoint base on date picker values */
-   loadMonthDataFromEndpoint = async (month, year) => {
-      await transactionsApi
-         .getMonthData("debitDistribution", { month, year })
-         .then((res) => {
-            if (res.status === 200) {
-               const { debitDistribution, transactionsList } = res.data;
-               this.setState({ transactionsList: transactionsList.data, debitDistribution });
-            }
-            if (res.status === 204) {
-               this.setState({ transactionsList: [], debitDistribution: {} });
-            }
-         })
-         .catch((err) => {
-            //TODO handle GET transactions list Error
-            throw err;
-         });
-   };
-
-   //TODO: change class name for list view container
-   render() {
-      return (
-         <div className="statistics-page">
-            <div className="section statistics-page__header">
-               <h2>תמונת מצב חודשית</h2>
-               <div className="statistics-page__month-year-selection">
-                  <SelectField
-                     value={this.state.datePicker.month}
-                     options={[
-                        { label: "ינואר", value: 1 },
-                        { label: "פברואר", value: 2 },
-                        { label: "מרץ", value: 3 },
-                        { label: "אפריל", value: 4 },
-                        { label: "מאי", value: 5 },
-                        { label: "יוני", value: 6 },
-                        { label: "יולי", value: 7 },
-                        { label: "אוגוסט", value: 8 },
-                        { label: "ספטמבר", value: 9 },
-                        { label: "אוקטובר", value: 10 },
-                        { label: "נובמבר", value: 11 },
-                        { label: "דצמבר", value: 12 },
-                     ]}
-                     config={{ fieldLabel: "בחר חודש", inputName: "month", displayMode: "row" }}
-                     onChangeCallback={this.onInputChange}
-                  />
-                  <SelectField
-                     value={this.state.datePicker.year}
-                     options={[
-                        { label: 2020, value: 2020 },
-                        { label: 2021, value: 2021 },
-                     ]}
-                     config={{ fieldLabel: "בחר שנה", inputName: "year", displayMode: "row" }}
-                     onChangeCallback={this.onInputChange}
-                  />
-               </div>
-            </div>
-
-            <div className="section">
-               <h2>התפלגות הוצאות לפי קטגוריה</h2>
-
-               <PieGraphHooks data={this.state.debitDistribution} />
-            </div>
-
-            <div className="list-container">
-               <div className="test-select">
-                  <SelectField
-                     value="חשבונות"
-                     options={[
-                        { label: "תאריך", value: "חשבונות" },
-                        { label: "אמצעי תשלום", value: "קניות" },
-                        { label: "קטגוריה", value: "בילויים" },
-                        { label: "סכום", value: "בילויים" },
-                     ]}
-                     config={{ fieldLabel: "מיין לפי", inputName: "test", displayMode: "row" }}
-                     onChangeCallback={this.onInputChange}
-                  />
-               </div>
-
-               <TransactionsList transactionsListData={this.state.transactionsList} isEditableList={false} />
+   return (
+      <div className="statistics-page">
+         <div className="section statistics-page__header">
+            <h2>תמונת מצב חודשית</h2>
+            <div className="statistics-page__month-year-selection">
+               <SelectField
+                  value={datePicker.month}
+                  options={[
+                     { label: "ינואר", value: 1 },
+                     { label: "פברואר", value: 2 },
+                     { label: "מרץ", value: 3 },
+                     { label: "אפריל", value: 4 },
+                     { label: "מאי", value: 5 },
+                     { label: "יוני", value: 6 },
+                     { label: "יולי", value: 7 },
+                     { label: "אוגוסט", value: 8 },
+                     { label: "ספטמבר", value: 9 },
+                     { label: "אוקטובר", value: 10 },
+                     { label: "נובמבר", value: 11 },
+                     { label: "דצמבר", value: 12 },
+                  ]}
+                  config={{ fieldLabel: "בחר חודש", inputName: "month", displayMode: "row" }}
+                  onChangeCallback={onInputChange}
+               />
+               <SelectField
+                  value={datePicker.year}
+                  options={[{ label: 2020, value: 2020 }]}
+                  config={{ fieldLabel: "בחר שנה", inputName: "year", displayMode: "row" }}
+                  onChangeCallback={onInputChange}
+               />
             </div>
          </div>
-      );
-   }
-}
+
+         <div className="section">
+            <h2>התפלגות הוצאות לפי קטגוריה</h2>
+
+            <PieGraphHooks data={monthData.metadata} />
+         </div>
+
+         <div className="list-container">
+            <div className="test-select">
+               <SelectField
+                  value={sortByPicker}
+                  options={[
+                     { label: "תאריך", value: "date" },
+                     { label: "אמצעי תשלום", value: "paymentMethod" },
+                     { label: "קטגוריה", value: "category" },
+                     { label: "תיאור", value: "description" },
+                     { label: "סכום", value: "totalPayment" },
+                  ]}
+                  config={{ fieldLabel: "מיין לפי", inputName: "sortBy", displayMode: "row" }}
+                  onChangeCallback={onInputChange}
+               />
+            </div>
+
+            <TransactionsList transactionsListData={sortedTransactionList} isEditableList={false} />
+         </div>
+      </div>
+   );
+};
 
 export default StatisticsAndDataPage;
