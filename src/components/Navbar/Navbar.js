0dd9dadfd4236ac66c595bb6c2eabf98e8cb8dc8
ronitebul1211 <ronitebul1211@gmail.com>
2021-01-25 00:02:44 +0200

    Navbar: extract private link to separate inner component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 import AuthenticationButton from '../_Auth/AuthenticationButton';
@@ -12,17 +12,21 @@ export default function Navbar() {
          <Link className="navbar__link" to="/">
             דף הבית
          </Link>
-         {isAuthenticated ? (
-            <React.Fragment>
-               <Link className="navbar__link" to="/current-month">
-                  תקציב חודשי
-               </Link>
-               <Link className="navbar__link" to="/statistics">
-                  נתונים וסטטיסטיקה
-               </Link>
-            </React.Fragment>
-         ) : null}
+         {isAuthenticated ? <PrivateLink /> : null}
          <AuthenticationButton className="navbar__link" />
       </div>
+   );
+}
+
+function PrivateLink() {
+   return (
+      <Fragment>
+         <Link className="navbar__link" to="/current-month">
+            תקציב חודשי
+         </Link>
+         <Link className="navbar__link" to="/statistics">
+            נתונים וסטטיסטיקה
+         </Link>
+      </Fragment>
    );
 }
